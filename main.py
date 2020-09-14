ad8261014a343f65c03fec3027b9c8a9bcc2a890
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-09-14 12:34:23 +0000

    Day 2

--- main.py
+++ main.py
@@ -1,91 +1,74 @@
-# N = list(map(int, input("Enter array: ").split(",")))
 
-def solve(n):
-  arr = []
-  for i in range(1,n+1):
-    if(i%3==0 and i%5==0):
-      arr.append("FizzBuzz")
-    elif(i%3==0):
-      arr.append("Fizz")
-    elif(i%5==0):
-      arr.append("Buzz")
-    else:
-      arr.append(i)
-  ans = list(map(str,arr))
-  return ans
+# def takeInput():
+#   cases = int(input())
+#   while cases > 0:
+#     size = int(input())
+#     arr = list(map(int, input().split()))
+#   # num = int(input())
+#     print(mean(arr))
+#     cases -=1
 
-# print(solve(N))
+#code
+# import math
+549755813888
+# print(round(math.log(0,2),1).is_integer())
 
-def maxProfit(prices):
-    max_profit, min_price = 0, float('inf')
-    for price in prices:
-        min_price = min(min_price, price)
-        profit = price - min_price
-        max_profit = max(max_profit, profit)
-    return max_profit
-
-# Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-  
-def maxVal(arr):
-  # length = len(arr)
-  max_count = 0
-  for num in arr:
-    count = arr.count(num)
-    prev = max_count
-    max_count = max(max_count,count)
+def rotation(a,r):
+    i = 0
+    while i > int(r):
+        rem = a.pop(0)
+        a.append(rem)
+        i += 1
+    return " ".join(map(str, a))
     
-    if(max_count > prev):
-      value = num
-  return value
-
-# print(maxVal(N))
-def majorityElement(nums):
-  dic = {}
-  for num in nums:
-    if num not in dic:
-      dic[num] = 1
-    if dic[num] > len(nums)//2:
-      print(dic)
-      return num
-    else:
-      dic[num] += 1 
-
-# print(majorityElement(N))  
-
-def calc(arrayVal):
-  product =1
-  for val in arrayVal:
-    product *= val
-  return product
+def missing(arr,n):
+  return sum(range(n+1)) - sum(arr)
 
 
 
-def composite(num):
-    dic = {}
-    # arr =[]
-    for i in range(2,num+1):
-      dic[i] = 1
-      for j in range(2,num+1):
-            if(i%j==0):
-                dic[i] +=1
-    for k, v in dic.items():
-      if(v>2):
-        print(k, end = " ")
+def power(num):
+    if(num > 0):
+      # return "YES" if round(math.log(num, 2),2).is_integer() else "NO"
+      pass
+
+
+def majority(arr,n):
+    for i in range(n):
+        if(arr.count(i) > n//2):
+            return i
+    return -1
+
+
     
-        
-def mean(arrVal):
-  sum = 0
-  for val in arrVal:
-    sum+=val
-    result = sum/len(arrVal)
-  return int(result)
+# for _ in range(int(input())):
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#     print(majority(arr, n))
+
+def subArraySum(arr, n, sum): 
+    for i in range(n): 
+        curr_sum = arr[i] 
+        j = i + 1
+        while j <= n: 
+            if curr_sum == sum: 
+               print(i+1, (j-1)+1) 
+               return ""
+                  
+            if curr_sum > sum or j == n: 
+                break
+              
+            curr_sum = curr_sum + arr[j] 
+            j += 1
+    return 0
 
 
-def takeInput():
-  cases = int(input())
-  while cases > 0:
-    size = int(input())
+
+
+cases = int(input())
+while cases > 0:
+    size, sums= input().split()
     arr = list(map(int, input().split()))
-  # num = int(input())
-    print(mean(arr))
+    # num = it(input()),
+    print(subArraySum(arr,int(size),int(sums)))
     cases -=1
+
